dfab5ccedc82665f7bf46af43738f37c14b441cb
williamellings <wille.ellings@hotmail.com>
2025-10-24 20:02:41 +0200

    Add project files.

---
+++ MetodManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Collections
+{
+    public class MetodManager
+    {
+
+    
+
+    public void Reverse()
+        {
+            List<string> words = new List<string> { "äpple", "banan", "citron" };
+            words.Reverse();
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using Advanced_Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        MetodManager metodManager = new MetodManager();
+        bool körMeny = true;
+
+        while (körMeny)
+        {
+            Console.Clear();
+            Console.WriteLine("=== Meny med 5 Övningar ===");
+            Console.WriteLine("1. Vänd Ord");
+            Console.WriteLine("2. Unika Namn");
+            Console.WriteLine("3. Kortaste och Längsta Ord");
+            Console.WriteLine("4. Hitta Jämna Tal");
+            Console.WriteLine("5. Ta Bort Korta Ord");
+            Console.WriteLine("6. Avsluta");
+            Console.Write("\nVälj ett alternativ: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    metodManager.Reverse();
+                    break;
+                case "2":
+                    UnikaNamn();
+                    break;
+                case "3":
+                    KortOchLångtOrd();
+                    break;
+                case "4":
+                    JämnaTal();
+                    break;
+                case "5":
+                    TaBortKortaOrd();
+                    break;
+                case "6":
+                    körMeny = false;
+                    Console.WriteLine("Programmet avslutas...");
+                    break;
+                default:
+                    Console.WriteLine("Fel val, försök igen!");
+                    break;
+            }
+
+            if (körMeny)
+            {
+                Console.WriteLine("\nTryck på valfri tangent för att återgå till menyn...");
+                Console.ReadKey();
+            }
+        }
+    }
